4355c43221cee851aa4cb605ee714a4106b77801
Amine El Ouassouli <a.elouassouli@gmail.com>
2023-07-22 17:03:05 +0200

    fix: pymodule name

--- tests/algebra/intervals/test_union.py
+++ tests/algebra/intervals/test_union.py
@@ -1,12 +1,12 @@
 from timewarp.algebra import interval_union
 from timewarp.models import TimeInterval
+from timewarp.timewarp import sum_as_string
 
 
 def test_interval_union(interval_a, interval_b):
-    assert interval_union(
-        interval_a,
-        interval_b,
-    ) == [
+    assert interval_union(interval_a, interval_b,) == [
         TimeInterval(start_timestamp=0, end_timestamp=1),
         TimeInterval(start_timestamp=2, end_timestamp=3),
     ]
+
+    assert sum_as_string(1, 2) == "3"

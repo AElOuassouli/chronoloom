5d6e1de522e66eed7205d7c746357922b6428a15
Amine El Ouassouli <a.elouassouli@gmail.com>
2023-07-22 17:40:56 +0200

    style: black fix

--- tests/algebra/intervals/test_union.py
+++ tests/algebra/intervals/test_union.py
@@ -4,7 +4,10 @@ from timewarp.timewarp import sum_as_string
 
 
 def test_interval_union(interval_a, interval_b):
-    assert interval_union(interval_a, interval_b,) == [
+    assert interval_union(
+        interval_a,
+        interval_b,
+    ) == [
         TimeInterval(start_timestamp=0, end_timestamp=1),
         TimeInterval(start_timestamp=2, end_timestamp=3),
     ]
